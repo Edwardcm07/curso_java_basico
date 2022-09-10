569fba76708ce6335055d6670713396a9b6052f1
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-09-10 10:56:17 -0500

    operadores

--- 4.operadores/operadores/src/App.java
+++ 4.operadores/operadores/src/App.java
@@ -1,5 +1,49 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // operadores
+
+        // aritmeticos
+
+        // +,-, /, *, %,
+        int num1 = 1;
+        int num2 = 2;
+        int resultado = num1 + num2;
+        resultado = num1 + num2;
+        int resultado2 = num1 * num2;
+        double resultado3 = num1 / num2;
+
+        // logicos, ralación, comparacion, booleanos
+
+        /*
+         * >
+         * <
+         * >=
+         * <=
+         * == se usa si un dato es igual a otro
+         * != se usa si un dato es diferente a otro
+         * && si se cumple una condición y otra condición (and)
+         * || si se cumple una condición u otra condición (or)
+         * ! negacion ejemplo !true=falso
+         */
+
+        // asignacion
+
+        /*
+         * =
+         * +=
+         * =
+         * -=
+         * /=
+         * %=
+         */
+
+        // incremento
+        // ++
+
+        // decremento
+        // --
+
+        // concatenación
+        // +
     }
 }
