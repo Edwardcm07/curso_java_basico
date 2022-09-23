0b0376ed5b26e9c28bfc2658b01f7a5e953d825e
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-09-22 21:26:56 -0500

    Bucle for

---
+++ 7.bucle_for/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        int contador;
+        for (int i = 0; i < 10; i++) {// condicion de comienzo; condicion de parada, condicion de paso
+            System.out.println(i + " Hola Mundo");// acción
+        }
+        System.out.println("Fin");
+
+        // ejemplo inventado por mi
+        for (int i = 1; i <= 10; i++) {
+            contador = i + 2;
+            System.out.println(contador);
+        }
+    }
+}
