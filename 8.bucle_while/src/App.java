e0182220f8075b4e2986fcbd7d09be0f0dfa5bc6
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-09-28 20:53:17 -0500

    continue y break

--- 8.bucle_while/src/App.java
+++ 8.bucle_while/src/App.java
@@ -4,8 +4,21 @@ public class App {
         int count = 0;
 
         while (count < 10) {
+
+            /*
+             * CICLO WHILE
+             * System.out.println("Hola como estan " + count);
+             * count++;// condicion
+             * }
+             */
+
+            // PALABRAS RESERVADAS continue y break
+            count++;
+            if (count == 6)
+                break;// rompe el flujo de ejecucion y rompe el bucle
+            // continue;//salta el valor ó y continua a la siguiente iteración
+
             System.out.println("Hola como estan " + count);
-            count++;// condicion
         }
         System.out.println("FIN del bucle");
     }
