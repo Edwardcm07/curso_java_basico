c58a2c5dbadc447d3fa209ef40aeefe695e823fe
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-09-28 20:18:35 -0500

    commit bucle while

---
+++ 7.bucle_while/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println("Hola como estan " + count);
+            count++;// condicion
+        }
+        System.out.println("FIN del bucle");
+    }
+}
