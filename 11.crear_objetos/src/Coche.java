cfe56d7708beddcf11da9d525de83b8165000185
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-10-04 21:07:50 -0500

    objetos

---
+++ 11.crear_objetos/src/Coche.java
@@ -0,0 +1,31 @@
+// creación de la clase
+
+public class Coche {
+
+    // atributos que varían para cada objeto coche
+    String color;
+    String fabricante;
+    String modelo;
+    Double peso;
+    Double largo;
+    Integer velocidad = 0;
+
+    // constructor: El constructor recibe una serie de parámetros (lo que está entre
+    // paréntesis) que nos permitirán crear o instanciar el objeto coche.
+
+    public Coche(String color, String fabricante, String modelo, Double peso, Double largo) {
+        this.color = color;
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.peso = peso;
+        this.largo = largo;
+    }
+    // La notación this. indica que el objeto que se crea a partir de la clase Coche
+    // hace referencia a las propiedades definidas en la declaración de la clase.
+
+    // comportamientos o métodos
+
+    public void acelerar() {
+        this.velocidad = velocidad;
+    }
+}
---
+++ 11.crear_objetos/src/CocheMain.java
@@ -0,0 +1,14 @@
+
+//la clase Main es el punto de partida para nuestro programa que creará un objeto coche
+
+public class CocheMain {
+    public static void main(String[] args) {
+        // creación del objeto cocherojo
+        Coche cocherojo = new Coche("rojo", "honda", "civic", 1430.45, 5.4);
+
+        // utilización del método creado en la clase Coche, al cual le actualizamos un
+        // nuevo valor
+        cocherojo.acelerar = 50;
+
+    }
+}
