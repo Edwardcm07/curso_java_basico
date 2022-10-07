75fc58434773bab2bd9ea690ee08dd2ffa576119
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-10-06 20:58:24 -0500

    herencia cambio del archivo Furgoneta

--- 12.herencia/src/furgoneta.java
+++ 12.herencia/src/furgoneta.java
@@ -1,8 +1,8 @@
-public class furgoneta extends Coche {
+public class Furgoneta extends Coche {
     private int capacidad_carga;
     private int plazas_extra;
 
-    public furgoneta(int plazas_extra, int capacidad_carga) {
+    public Furgoneta(int plazas_extra, int capacidad_carga) {
         super(); // llama al constructor de la clase padre
         this.capacidad_carga = capacidad_carga;
         this.plazas_extra = plazas_extra;
