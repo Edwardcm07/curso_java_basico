39135bcaa37070517694961e5d84188c8a122aa3
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-12-14 21:15:38 -0500

    Fase segunda commit 2

--- Segunda_Parte/Prueba/prueba/src/AdiosMundo.java
+++ Segunda_Parte/Prueba/prueba/src/AdiosMundo.java
@@ -1,8 +1,25 @@
-import javax.sound.sampled.SourceDataLine;
-
+/**
+ * clase de prueba para crear un programa basico de java
+ */
 public class AdiosMundo {
+
+    /**
+     * Punto de entrada a la aplicación JAVA
+     * Permite ejecutar la aplicación
+     * 
+     * @param args - argumentos de entrada para la aplicación
+     */
     public static void main(String[] args) {
-        System.out.println("Adios mundo cruel");
+        // esto es un comentario
+        System.out.println("Adios mundo cruel");// esto es otro comentario
+
+        /*
+         * esto es un comentarioi de multilinea
+         * 1- crear función suma
+         * 2- crear variables tipo numrico
+         * 3- utilizar la funcion suma para calcular la suma de los dos números
+         */
+
     }
 
 }
---
+++ Segunda_Parte/Prueba/prueba/src/TiposDeDatos.java
@@ -0,0 +1,25 @@
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        // 1. númericos
+
+        // 1.1 enteros
+        byte variable1 = 5;
+        short Variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        long numeroTelefono = 666555444;
+        // 1.2 decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+        double preciosilla = 49.99;
+
+        // 2. booleanos
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. texto
+        char variable9 = 'a';
+        String variable10 = "Hola como estas.....";
+
+    }
+}
